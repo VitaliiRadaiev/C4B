86fdda7c289f580e43fea09067eb0518157449c7
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-11-25 11:51:07 +0200

    fix

--- #src/common/news-list/news-list.js
+++ #src/common/news-list/news-list.js
@@ -1,11 +1,22 @@
 {
-    let galleryCardTextAll = document.querySelectorAll('.gallery-cards__text');
-    if(galleryCardTextAll.length) {
-        galleryCardTextAll.forEach(text => {
+    let newsTextAll = document.querySelectorAll('.news-list__text');
+    if(newsTextAll.length) {
+        newsTextAll.forEach(text => {
             if(text.closest('.big')) {
                 trimString(text, 157);
             } else {
                 trimString(text, 80);
+            }
+        })
+    }
+
+    let newsTitleAll = document.querySelectorAll('.news-list__title');
+    if(newsTitleAll.length) {
+        newsTitleAll.forEach(text => {
+            if(text.closest('.big')) {
+                trimString(text, 50);
+            } else {
+                trimString(text, 32);
             }
         })
     }
--- dist/js/app.js
+++ dist/js/app.js
@@ -317,13 +317,24 @@ function trimString(el, stringLength = 0) {
 }
 ;
 	{
-    let galleryCardTextAll = document.querySelectorAll('.gallery-cards__text');
-    if(galleryCardTextAll.length) {
-        galleryCardTextAll.forEach(text => {
+    let newsTextAll = document.querySelectorAll('.news-list__text');
+    if(newsTextAll.length) {
+        newsTextAll.forEach(text => {
             if(text.closest('.big')) {
                 trimString(text, 157);
             } else {
                 trimString(text, 80);
+            }
+        })
+    }
+
+    let newsTitleAll = document.querySelectorAll('.news-list__title');
+    if(newsTitleAll.length) {
+        newsTitleAll.forEach(text => {
+            if(text.closest('.big')) {
+                trimString(text, 50);
+            } else {
+                trimString(text, 32);
             }
         })
     }
